1dc55a22d9a1feef17464ad4a4f3870dd1a1ec37
bruno <brunomestres@gmail.com>
2021-08-17 21:17:13 -0300

    first commit

---
+++ src/components/Timer/index.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+
+function Timer() {
+  const [time, setTime] = useState(0);
+
+  // Your code should be here
+  
+  
+  useEffect(()=>{
+    setTimeout(() => {
+      setTime(() => time + 1)
+    } , 1000)
+  },[time])
+
+
+  return (
+    <div>
+        <p>
+          Time elapsed: {time}s
+        </p>
+    </div>
+  );
+}
+
+export default Timer;
